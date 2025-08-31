127f22014727e5ec0411cfadd8093518f87903c7
Nahom <nahomberhane101@gmail.com>
2025-08-31 14:41:14 +0200

    remvoed PPR since it wont deploy when on !

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   experimental: {
-    ppr: 'incremental',
+    // ppr: 'incremental',
   },
   images: {
     domains: ['cdn.sanity.io'],
